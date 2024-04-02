89daa7f44d3ea9ef512c4344f8d6a1e93d773e9f
gonzalolavin99 <gonzalolavin99@gmail.com>
2024-04-01 22:50:47 -0300

    UPDATE: MEJORA VISUAL VIEW DE TÉRMINOS

--- WEB/src/views/Terminos.jsx
+++ WEB/src/views/Terminos.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+
 
 const Terminos = () => {
   return (
-    <div>Al cumplirse la condición, mínima de ticket vendidos, del producto y/o servicio, se iniciara un contador automático ( fecha y hora), donde se estipulara desde 5 a 10 días de corridos para la adjudicación del producto y/o servicios, entre los usuarios que hayan y realicen compras de ticket, dentro del plazo establecido en cada Sorteo.
-
+    <div className="terminos-container">
+      <h1 className="terminos-titulo">Términos y Condiciones</h1>
+      <div className="terminos-contenido">
+        <p>
+          Al cumplirse la condición mínima de ticket vendidos del producto y/o servicio, se iniciará un contador automático (fecha y hora), donde se estipularán de 5 a 10 días corridos para la adjudicación del producto y/o servicios, entre los usuarios que hayan realizado compras de ticket dentro del plazo establecido en cada Sorteo.
+        </p>
+        <p>
+          Cada Sorteo acepta una cantidad "N" de tickets a comprar por usuarios registrados, con un tope máximo de tickets declarado por cada sorteo en el portal.
+        </p>
+        <p>
+          La información solicitada en Yo Sorteo corresponde a datos personales del usuario que se registre en el portal de Sorteos mediante un formulario de registro. Dicha información será utilizada para corroborar la identidad del adjudicador del Sorteo una vez este haya finalizado.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Terminos
+export default Terminos;
