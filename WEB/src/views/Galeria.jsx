64142589d470df32551ad08722f02c8e9ab67fbe
gonzalolavin99 <gonzalolavin99@gmail.com>
2024-04-08 20:49:21 -0400

    UPDATE: Se modifica formulario de datos del cliente  y se muestran en  view de compra-exitosa

--- WEB/src/views/Galeria.jsx
+++ WEB/src/views/Galeria.jsx
@@ -3,12 +3,15 @@ import useMediaQuery from "react-responsive";
 import CardGalería from "../components/CardGalería";
 
 const Galeria = () => {
-  console.log("Renderizando galería");
+  // Verificar el tamaño de pantalla para determinar el número de columnas en la grilla
+
   const isDesktop = useMediaQuery({ minWidth: 992 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
   const columnas = isDesktop ? 4 : isTablet ? 3 : isMobile ? 2 : 1;
+
+  // Actualizar las columnas del grid cuando cambie el tamaño de la pantalla
 
   useEffect(() => {
     const handleResize = () => {
@@ -19,11 +22,14 @@ const Galeria = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  // Estados para manejar la imagen seleccionada y el cursor
   const [imagenSeleccionada, setImagenSeleccionada] = useState(null);
   const [indiceImagen, setIndiceImagen] = useState(0);
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
   const imageRef = useRef(null);
 
+  // Arreglo de URLs de las imágenes
   const imagenes = [
     "https://i.pinimg.com/originals/45/a2/bd/45a2bd8ff5413360222defb7c69a7a93.jpg",
     "https://i.pinimg.com/originals/f8/49/3e/f8493e3737a88f214cbb2bdbb61da009.jpg",
@@ -41,25 +47,30 @@ const Galeria = () => {
     "https://i.pinimg.com/originals/76/01/0e/76010e9095791df99b7c688fadf22b00.jpg",
   ];
 
+  // Manejar el clic en una imagen de la galería
   const handleImagenClick = (index) => {
     setImagenSeleccionada(true);
     setIndiceImagen(index);
   };
 
+  // Cerrar el carrusel de imagen seleccionada
   const handleCerrarCarrusel = () => {
     setImagenSeleccionada(null);
   };
 
+  // Manejar el cambio a la siguiente imagen en el carrusel
   const handleSiguienteImagen = (e) => {
     e.stopPropagation(); // Evitar propagación del evento
     setIndiceImagen((indiceImagen + 1) % imagenes.length);
   };
 
+  // Manejar el cambio a la imagen anterior en el carrusel
   const handleAnteriorImagen = (e) => {
     e.stopPropagation(); // Evitar propagación del evento
     setIndiceImagen((indiceImagen - 1 + imagenes.length) % imagenes.length);
   };
 
+  // Manejar el movimiento del cursor sobre la imagen en el carrusel
   const handleCursorMove = (e) => {
     const rect = e.currentTarget.getBoundingClientRect();
     const x = e.clientX - rect.left;
@@ -70,11 +81,14 @@ const Galeria = () => {
       if (e.type === "mousemove") {
         const maxScale = 2; // Define el nivel máximo de zoom permitido
         const currentScale =
-          imageRef.current.getBoundingClientRect().width /
-          imageRef.current.offsetWidth;
+          imageRef.current.getBoundingClientRect().width / imageSize.width;
 
         if (currentScale < maxScale) {
-          imageRef.current.style.transformOrigin = `${x}px ${y}px`;
+          // Calcular los porcentajes de posición del cursor en relación al tamaño de la imagen
+
+          imageRef.current.style.transformOrigin = `${
+            x / (imageSize.width / 100)
+          }% ${y / (imageSize.height / 100)}%`;
         }
       } else if (e.type === "mouseenter") {
         imageRef.current.style.transformOrigin = "center center";
@@ -82,6 +96,12 @@ const Galeria = () => {
         imageRef.current.style.transformOrigin = "center center";
       }
     }
+  };
+
+  // Manejar la carga de la imagen en el carrusel
+  const handleImageLoad = (e) => {
+    const { width, height } = e.target;
+    setImageSize({ width, height });
   };
 
   return (
@@ -97,27 +117,28 @@ const Galeria = () => {
       </div>
 
       {imagenSeleccionada && (
-      <div className="carrusel" onClick={handleCerrarCarrusel}>
-        <div className="carrusel-contenido">
-          <button className="anterior" onClick={handleAnteriorImagen}>
-            &lt;
-          </button>
-          <img
-            src={imagenes[indiceImagen]}
-            alt="Imagen seleccionada"
-            onMouseMove={handleCursorMove}
-            onMouseEnter={handleCursorMove}
-            onMouseLeave={handleCursorMove}
-            ref={imageRef}
-            style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-          />
-          <button className="siguiente" onClick={handleSiguienteImagen}>
-            &gt;
-          </button>
+        <div className="carrusel" onClick={handleCerrarCarrusel}>
+          <div className="carrusel-contenido">
+            <button className="anterior" onClick={handleAnteriorImagen}>
+              &lt;
+            </button>
+            <img
+              src={imagenes[indiceImagen]}
+              alt="Imagen seleccionada"
+              onMouseMove={handleCursorMove}
+              onMouseEnter={handleCursorMove}
+              onMouseLeave={handleCursorMove}
+              onLoad={handleImageLoad}
+              ref={imageRef}
+              style={{ width: "100%", height: "100%", objectFit: "contain" }}
+            />
+            <button className="siguiente" onClick={handleSiguienteImagen}>
+              &gt;
+            </button>
+          </div>
         </div>
-      </div>
-    )}
-  </div>
+      )}
+    </div>
   );
 };
 
