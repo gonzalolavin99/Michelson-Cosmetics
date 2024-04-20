21534b1556d8721367c547129e12653e82febaf7
gonzalolavin99 <gonzalolavin99@gmail.com>
2024-04-20 18:43:42 -0400

    UPDATE:  Solución problemas de vista de premios

--- WEB/src/views/Premios/Premios.jsx
+++ WEB/src/views/Premios/Premios.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Modal, Button, Carousel } from "antd";
+import { NavLink } from 'react-router-dom';
+
 import "./premios.css"; // Asegúrate de importar tus estilos CSS aquí
 
 const Premios = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [visible, setVisible] = useState(false);
   const [selectedPremio, setSelectedPremio] = useState(null);
 
   const premios = [
@@ -13,21 +15,21 @@ const Premios = () => {
       descripcion: "Descripción corta del primer premio.",
       imagenes: [
         "https://i.pinimg.com/736x/45/a2/bd/45a2bd8ff5413360222defb7c69a7a93.jpg",
+        "https://i.pinimg.com/736x/9f/0d/99/9f0d99956cfe2dc486b9434f3587f8c9.jpg",
         "https://i.pinimg.com/736x/33/cb/82/33cb820dae66900f1def6f8223033ba9.jpg",
-        "imagen3.jpg",
       ],
       descripcionLarga: "Descripción más detallada del primer premio.",
     },
     {
       id: 2,
-      nombre: "Segundo Premio",
-      descripcion: "Descripción corta del segundo premio.",
+      nombre: "Moschino Couture Bag",
+      descripcion: "Premio segundo lugar",
       imagenes: [
         "https://i.pinimg.com/736x/23/33/71/2333712fabe6078f28d05f6e1c181bfb.jpg",
         "https://i.pinimg.com/736x/5a/8d/0b/5a8d0b8872b360bb64e6d58ea95e17fd.jpg",
-        "imagen6.jpg",
+        "https://i.pinimg.com/736x/2a/b8/28/2ab828acbb8b2d1403caf24b31631966.jpg",
       ],
-      descripcionLarga: "Descripción más detallada del segundo premio.",
+      descripcionLarga: "Esta bolsa de mano Moschino Couture es un accesorio imprescindible para cualquier ocasión. Con un diseño de corazón en rosa, cierre de cremallera y magnético, y detalles de hardware dorado, es tanto elegante como práctica. Hecha de poliuretano de alta calidad y fabricada en Italia, es perfecta para viajes, fiestas, trabajo o eventos formales.",
     },
     {
       id: 3,
@@ -42,16 +44,13 @@ const Premios = () => {
     },
   ];
 
-  const openModal = (premio) => {
+  const showModal = (premio) => {
     setSelectedPremio(premio);
-    setIsOpen(true);
-    console.log(selectedPremio);
-    console.log(isOpen);
+    setVisible(true);
   };
 
-  const closeModal = () => {
-    setSelectedPremio(null);
-    setIsOpen(false);
+  const handleCancel = () => {
+    setVisible(false);
   };
 
   return (
@@ -66,43 +65,50 @@ const Premios = () => {
           <div className="card-content">
             <h3 className="card-title">{premio.nombre}</h3>
             <p className="card-description">{premio.descripcion}</p>
-            <button onClick={() => openModal(premio)} className="btn-pink">
+            <Button className="btn-pink" onClick={() => showModal(premio)} type="primary">
               Ver Detalles
-            </button>
+            </Button>
           </div>
         </div>
       ))}
 
-      {isOpen && selectedPremio && (
-        <div className="modal-overlay" onClick={closeModal}>
-          <div className="modal">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-            <h2>{selectedPremio.nombre}</h2>
-            <div className="modal-content">
-              {/* Carrusel de imágenes */}
-              <div className="modal-carousel">
+<Modal
+  title={selectedPremio && selectedPremio.nombre}
+  visible={visible}
+  onCancel={handleCancel}
+  bodyStyle={{ backgroundColor: '#ffc0cb' }} // Cambiar color de fondo del modal
+  footer={[
+    <Button key="back" onClick={handleCancel}>
+      Cerrar
+    </Button>,
+    <Button key="buy">
+      <NavLink to="/compra" className="btn-pink">
+        Ir a Comprar
+      </NavLink>
+    </Button>,
+  ]}
+>
+        <div className="modal-content">
+          {selectedPremio && (
+            <>
+              <Carousel autoplay>
                 {selectedPremio.imagenes.map((imagen, index) => (
-                  <img
-                    key={index}
-                    src={imagen}
-                    alt={`Imagen ${index + 1}`}
-                    className="modal-image"
-                  />
+                  <div key={index}>
+                    <img
+                      src={imagen}
+                      alt={`Imagen ${index + 1}`}
+                      className="modal-image"
+                    />
+                  </div>
                 ))}
-              </div>
-              {/* Descripción larga */}
+              </Carousel>
               <div className="modal-description">
                 <p>{selectedPremio.descripcionLarga}</p>
-                <NavLink to="/compra" className="btn-pink">
-                  Ir a Comprar
-                </NavLink>
               </div>
-            </div>
-          </div>
+            </>
+          )}
         </div>
-      )}
+      </Modal>
     </div>
   );
 };
