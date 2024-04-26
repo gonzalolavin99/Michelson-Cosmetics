f7d8d5902fab447c6549d9e3775fac58043c9d00
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-04-25 22:53:48 -0400

    UPDATE: Cambios efectos vista comprar número

--- WEB/src/views/Premios/Premios.jsx
+++ WEB/src/views/Premios/Premios.jsx
@@ -91,7 +91,7 @@ const Premios = () => {
         <div className="modal-content">
           {selectedPremio && (
             <>
-              <Carousel autoplay  autoplaySpeed={1000}>
+              <Carousel autoplay  autoplaySpeed={800}>
                 {selectedPremio.imagenes.map((imagen, index) => (
                   <div key={index}>
                     <img
