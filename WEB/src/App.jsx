7c246f4efe978814fe01e1cf37733f30f7b82c5a
Leonardo Duque <leoartdev@gmail.com>
2024-04-29 00:04:49 -0400

    Se agregaron efectos al banner, tarjetas sobrepuestas al banner para mostrar premios

--- WEB/src/App.jsx
+++ WEB/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "./components/listadoExportaciones.js";
+import { Footer, Navbar } from "./components/listadoExportaciones.js";
 import Home from "./views/Home";
 import Galeria from "./views/Galeria";
 import Terminos from "./views/Terminos";
@@ -14,32 +14,36 @@ import CompraExitosa from "./views/CompraExitosa";
 import CompraFallida from "./views/CompraFallida";
 import TicketProvider from "./context/TicketContext"; // Importar TicketProvider
 import NotFound from "./views/NotFound.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <TicketProvider>
-      <div className="row">
-        <div className="col-12">
-          <Navbar />
+    <ChakraProvider>
+      <TicketProvider>
+        <div className="row">
+          <div className="col-12">
+            <Navbar />
+          </div>
         </div>
-      </div>
-      <div className="row mt-5">
-        <div className="col-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/galeria" element={<Galeria />} />
-            <Route path="/compra" element={<ComprarNumero />} />
-            <Route path="/carrito" element={<Carrito />} />
-            <Route path="/terminos" element={<Terminos />} />
-            <Route path="/premios" element={<Premios />} />
-            <Route path="/pruebaApi" element={<PruebaApi />} />
-            <Route path="/compra-exitosa" element={<CompraExitosa />} />
-            <Route path="/compra-fallida" element={<CompraFallida />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+        <div className="row mt-5">
+          <div className="col-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/galeria" element={<Galeria />} />
+              <Route path="/compra" element={<ComprarNumero />} />
+              <Route path="/carrito" element={<Carrito />} />
+              <Route path="/terminos" element={<Terminos />} />
+              <Route path="/premios" element={<Premios />} />
+              <Route path="/pruebaApi" element={<PruebaApi />} />
+              <Route path="/compra-exitosa" element={<CompraExitosa />} />
+              <Route path="/compra-fallida" element={<CompraFallida />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </div>
         </div>
-      </div>
-    </TicketProvider>
+      </TicketProvider>
+    </ChakraProvider>
   );
 };
 
--- WEB/src/components/listadoExportaciones.js
+++ WEB/src/components/listadoExportaciones.js
@@ -1,8 +1,9 @@
-export { Banner } from "./Banner.jsx";
+export { Banner } from "./banner/Banner.jsx";
 export { Navbar } from "./Bar.jsx";
 export { BuyButton } from "./BuyButton.jsx";
 export { Carrusel } from "./Carrusel.jsx";
-export { Footer } from "./Footer.jsx";
+export { Footer } from "./Footer/Footer.jsx";
 export { IgButton } from "./IgButton.jsx";
 export { Info } from "./Info.jsx";
+export { Minicard } from "./Minicard.jsx";
 // export { Nv } from "./Nv.jsx";
