9c1892b4935e8a5eb35dd72977f19be8f37d51a0
JesGoy <juan99s.m@gmail.com>
2024-05-15 23:20:33 -0400

    FIX: Enpoint notify

--- API-KHIPU/API-KHIPU/Routes/PaymentRoute.cs
+++ API-KHIPU/API-KHIPU/Routes/PaymentRoute.cs
@@ -1,6 +1,11 @@
 ﻿using API_KHIPU.Service;
 using Khipu.Api;
 using Khipu.Model;
+using System.Text.Json;
+using System.Runtime.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
 
 namespace API_KHIPU.Routes
 {
@@ -23,6 +28,20 @@ namespace API_KHIPU.Routes
                 return _paymentService.CreateNewPurchase(purchase);
 
             });
+
+            app.MapPost("/notifyPurchase", async (HttpRequest request, PaymentService _paymentService) =>
+            {
+                
+
+                
+                var form = await request.ReadFormAsync();
+                PaymentsResponse paymentsResponse = new PaymentsResponse() { 
+                    notification_token = form["notification_token"].ToString(),
+                    api_version = form["api_version"].ToString()
+                };
+                _paymentService.NotifyPurcharse(paymentsResponse);
+
+                return Results.Ok();            });
         }
 
 
@@ -42,5 +61,71 @@ namespace API_KHIPU.Routes
         public string paymentId { get; set; }
         public bool success { get; set; } = false;
     }
+    public class PaymentsResponse
+    {
+        public string notification_token { get; set; }
+        public string api_version { get; set; }
+    }
+        public class PaymentsResponse3
+    {
+    
+        public string payment_id { get; set; }
 
+        public string payment_url { get; set; }
+
+        public string simplified_transfer_url { get; set; }
+
+  
+        public string transfer_url { get; set; }
+
+        public string webpay_url { get; set; }
+
+
+        public string hites_url { get; set; }
+
+
+        public string payme_url { get; set; }
+
+        public string app_url { get; set; }
+
+        
+        public bool? ready_for_terminal { get; set; }
+
+
+        public string notification_token { get; set; }
+
+        public long? receiver_id { get; set; }
+
+        public DateTime? conciliation_date { get; set; }
+        public string subject { get; set; }
+
+        public double? amount { get; set; }
+        public string currency { get; set; }
+        public string status { get; set; }
+        public string status_detail { get; set; }
+        public string body { get; set; }
+        public string picture_url { get; set; }
+        public string receipt_url { get; set; }
+        public string return_url { get; set; }
+        public string cancel_url { get; set; }
+        public string notify_url { get; set; }
+        public string notify_api_version { get; set; }
+        public DateTime? expires_date { get; set; }
+        public List<string> attachment_urls { get; set; }
+        public string bank { get; set; }
+        public string bank_id { get; set; }
+        public string payer_name { get; set; }
+        public string payer_email { get; set; }
+        public string personal_identifier { get; set; }
+        public string bank_account_number { get; set; }
+        public bool? out_of_date_conciliation { get; set; }
+        public string transaction_id { get; set; }
+        public string custom { get; set; }
+        public string responsible_user_email { get; set; }
+        public bool? send_reminders { get; set; }
+        public bool? send_email { get; set; }
+        public string payment_method { get; set; }
+        public string funds_source { get; set; }
+       
+    }
 }
--- API-KHIPU/API-KHIPU/Service/PaymentService.cs
+++ API-KHIPU/API-KHIPU/Service/PaymentService.cs
@@ -2,6 +2,13 @@
 using Khipu.Api;
 using Khipu.Client;
 using Khipu.Model;
+using System.Net.Mail;
+using System.Net;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net.Http;
+using System.Text;
+using System;
 
 namespace API_KHIPU.Service
 {
@@ -9,11 +16,12 @@ namespace API_KHIPU.Service
     {
         private readonly IConfiguration _configuration;
         PaymentsApi khipu = new PaymentsApi();
-    
+        private readonly HttpClient _httpClient;
 
         public PaymentService(IConfiguration configuration) 
         { 
             _configuration = configuration;
+            _httpClient = new HttpClient();
         }
 
         public async Task<NewPurchaseResponse> CreateNewPurchase(NewPurchaseRequest purchase)
@@ -51,6 +59,41 @@ namespace API_KHIPU.Service
             {
                 
                return (NewPurchaseResponse)Results.BadRequest(ex.Message);
+            }
+
+        }
+        public async Task NotifyPurcharse(Routes.PaymentsResponse notiToken)
+        {
+            try
+            {
+                Configuration.ReceiverId = Convert.ToInt64(_configuration["ApiKhipu:recieved_id"]);
+                Configuration.Secret = _configuration["ApiKhipu:secret_key"];
+                Khipu.Model.PaymentsResponse purchase =  khipu.PaymentsGet(notiToken.notification_token);
+                var newp = new { pass = purchase.PaymentId, idPurchase = purchase.ReceiverId };
+                string jsonData = JsonSerializer.Serialize(newp);
+
+                // Crear el contenido de la solicitud
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                // Enviar la solicitud POST
+                HttpResponseMessage response = await _httpClient.PostAsync("http://35.183.244.41/ticket", content);
+
+                // Asegurarse de que la solicitud fue exitosa
+                response.EnsureSuccessStatusCode();
+
+                // Leer la respuesta
+                string responseBody = await response.Content.ReadAsStringAsync();
+               
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
             }
 
         }
