110d393b4ae191a5b1d6a8a6a352398165734594
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-05-26 18:45:55 -0400

    UPDATE: Mensaje de cargando al no cargar imágenes

---
+++ WEB/src/components/spinner/Spinner.jsx
@@ -0,0 +1,28 @@
+// Spinner.jsx
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const SpinnerContainer = styled.div`
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  border: 3px solid rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  border-top-color: #3498db;
+  animation: ${spin} 1s ease-in-out infinite;
+`;
+
+const Spinner = () => {
+  return <SpinnerContainer />;
+};
+
+export default Spinner;
--- WEB/src/views/Galeria.jsx
+++ WEB/src/views/Galeria.jsx
@@ -3,6 +3,24 @@ import useMediaQuery from "react-responsive";
 import CardGalería from "../components/CardGalería";
 
 const Galeria = () => {
+  
+  // Arreglo de URLs de las imágenes
+  const imagenes = [
+    "https://i.pinimg.com/originals/45/a2/bd/45a2bd8ff5413360222defb7c69a7a93.jpg",
+    "https://i.pinimg.com/originals/f8/49/3e/f8493e3737a88f214cbb2bdbb61da009.jpg",
+    "https://i.pinimg.com/originals/33/cb/82/33cb820dae66900f1def6f8223033ba9.jpg",
+    "https://i.pinimg.com/originals/9f/0d/99/9f0d99956cfe2dc486b9434f3587f8c9.jpg",
+    "https://i.pinimg.com/originals/e1/57/5c/e1575cb6c48b36548f41609a601a3fc0.jpg",
+    "https://i.pinimg.com/736x/e2/2c/0f/e22c0f19cc96ffec99b4449f8b0b525a.jpg",
+    "https://i.pinimg.com/736x/9a/d0/13/9ad013e9b645899dba9c0dba518ea10a.jpg",
+
+    "https://i.pinimg.com/736x/5c/04/79/5c0479879338eb89ca7d099fe2c85b75.jpg",
+    "https://i.pinimg.com/736x/e4/54/d3/e454d32bb89c176f4041d18974d28433.jpg",
+    "https://i.pinimg.com/736x/43/13/6a/43136a62dc4961c3525ed384315b1d0d.jpg",
+    "https://i.pinimg.com/736x/96/1d/f4/961df4c9fa9e1cd9d3d8200221c0ac4e.jpg",
+    "https://i.pinimg.com/originals/eb/70/8d/eb708df657317958227fd532224bf02e.jpg",
+    "https://i.pinimg.com/originals/76/01/0e/76010e9095791df99b7c688fadf22b00.jpg",
+  ];
   // Verificar el tamaño de pantalla para determinar el número de columnas en la grilla
 
   const isDesktop = useMediaQuery({ minWidth: 992 });
@@ -27,25 +45,9 @@ const Galeria = () => {
   const [indiceImagen, setIndiceImagen] = useState(0);
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
   const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
-  const imageRef = useRef(null);
+  const imageRef = useRef(null);const [imageLoaded, setImageLoaded] = useState(Array(imagenes.length).fill(false));
 
-  // Arreglo de URLs de las imágenes
-  const imagenes = [
-    "https://i.pinimg.com/originals/45/a2/bd/45a2bd8ff5413360222defb7c69a7a93.jpg",
-    "https://i.pinimg.com/originals/f8/49/3e/f8493e3737a88f214cbb2bdbb61da009.jpg",
-    "https://i.pinimg.com/originals/33/cb/82/33cb820dae66900f1def6f8223033ba9.jpg",
-    "https://i.pinimg.com/originals/9f/0d/99/9f0d99956cfe2dc486b9434f3587f8c9.jpg",
-    "https://i.pinimg.com/originals/e1/57/5c/e1575cb6c48b36548f41609a601a3fc0.jpg",
-    "https://i.pinimg.com/736x/e2/2c/0f/e22c0f19cc96ffec99b4449f8b0b525a.jpg",
-    "https://i.pinimg.com/736x/9a/d0/13/9ad013e9b645899dba9c0dba518ea10a.jpg",
-
-    "https://i.pinimg.com/736x/5c/04/79/5c0479879338eb89ca7d099fe2c85b75.jpg",
-    "https://i.pinimg.com/736x/e4/54/d3/e454d32bb89c176f4041d18974d28433.jpg",
-    "https://i.pinimg.com/736x/43/13/6a/43136a62dc4961c3525ed384315b1d0d.jpg",
-    "https://i.pinimg.com/736x/96/1d/f4/961df4c9fa9e1cd9d3d8200221c0ac4e.jpg",
-    "https://i.pinimg.com/originals/eb/70/8d/eb708df657317958227fd532224bf02e.jpg",
-    "https://i.pinimg.com/originals/76/01/0e/76010e9095791df99b7c688fadf22b00.jpg",
-  ];
+  
 
   // Manejar el clic en una imagen de la galería
   const handleImagenClick = (index) => {
@@ -71,22 +73,29 @@ const Galeria = () => {
   };
 
   // Manejar la carga de la imagen en el carrusel
-  const handleImageLoad = (e) => {
+  const handleImageLoad = (e, index) => {
     const { width, height } = e.target;
     setImageSize({ width, height });
+    setImageLoaded((prevImageLoaded) => {
+      const newImageLoaded = [...prevImageLoaded];
+      newImageLoaded[index] = true;
+      return newImageLoaded;
+    });
   };
 
   return (
     <div className="galeria">
-      <div className="galeria-grid">
-        {imagenes.map((imagen, index) => (
-          <CardGalería
-            key={index}
-            imagen={imagen}
-            onClick={() => handleImagenClick(index)}
-          />
-        ))}
-      </div>
+    <div className="galeria-grid">
+      {imagenes.map((imagen, index) => (
+        <CardGalería
+          key={index}
+          imagen={imagen}
+          onClick={() => handleImagenClick(index)}
+          isLoaded={imageLoaded[index]}
+          onLoad={(e) => handleImageLoad(e, index)}
+        />
+      ))}
+    </div>
 
       {imagenSeleccionada && (
         <div className="carrusel" onClick={handleCerrarCarrusel}>
